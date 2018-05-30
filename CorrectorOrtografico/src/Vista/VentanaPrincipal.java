79285afc373a98621cd4536152c4f6115edad0fb
alex <alwex@hotmail.es>
2018-05-30 15:35:58 +0200

    empezando correctorOrtografico

---
+++ CorrectorOrtografico/src/Vista/VentanaPrincipal.java
@@ -0,0 +1,10 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JButton marcarButton;
+    private JButton sustituirButton;
+    private JButton personalizadaButton;
+    private JTextArea texto;
+}
