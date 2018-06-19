81a13efc3c24b4c5190ce8fadeb76001372e6567
alex <alwex@hotmail.es>
2018-06-19 14:06:24 +0200

    propuesta algoritmo nlogn

--- NubePuntos/src/Controlador/ColocaN.java
+++ NubePuntos/src/Controlador/ColocaN.java
@@ -2,6 +2,8 @@ package Controlador;
 
 import Modelo.ModelInterface;
 
+import java.util.ArrayList;
+
 public class ColocaN implements ControlerInterface{
 
     private ModelInterface modelo;
@@ -11,18 +13,9 @@ public class ColocaN implements ControlerInterface{
     }
 
     public void distanciaMinima(){
-        int masCercano, cercano1 = 0, cercano2 = 0;
-        long distanciaMinima = Long.MAX_VALUE;
-        ordenarPuntos(0, modelo.getLenght() - 1); //Order by X O(log(n))
-        for (int punto = 0; punto < modelo.getLenght() - 1; punto++){
-            masCercano = minim(punto, 0, modelo.getLenght() - 1);
-            if (distancia(punto, masCercano) < distanciaMinima){
-                distanciaMinima = distancia(punto, masCercano);
-                cercano1 = punto;
-                cercano2 = masCercano;
-            }
-        }
-        modelo.setCercanos(cercano1, cercano2);
+        ordenarPuntosPorX(0, modelo.getLenght() - 1); //Order by X O(log(n))
+        int[] cercanos = obtenerCercanos(0, modelo.getLenght() - 1, (modelo.getLenght() - 1)/2);
+        modelo.setCercanos(cercanos[0], cercanos[1]);
     }
 
     private int minim(int elem, int i, int j){
@@ -49,19 +42,13 @@ public class ColocaN implements ControlerInterface{
         return modelo.distancia(elem, indx);
     }
 
-    private void ordenarPuntos(int lInd, int hInd){
+    private void ordenarPuntosPorX(int lInd, int hInd){
         int i = lInd;
         int j = hInd;
         // calculate pivot number, I am taking pivot as middle index number
         int pivot = modelo.getCoordenadaX(lInd+(hInd-lInd)/2);
         // Divide into two arrays
         while (i <= j) {
-            /**
-             * In each iteration, we will identify a number from left side which
-             * is greater then the pivot value, and also we will identify a number
-             * from right side which is less then the pivot value. Once the search
-             * is done, then we exchange both numbers.
-             */
             while (modelo.getCoordenadaX(i) < pivot) {
                 i++;
             }
@@ -76,9 +63,74 @@ public class ColocaN implements ControlerInterface{
             }
         }
         // call quickSort() method recursively
-        if (lInd < j)
-            ordenarPuntos(lInd, j);
-        if (i < hInd)
-            ordenarPuntos(i, hInd);
+        if (lInd < j) {
+            ordenarPuntosPorX(lInd, j);
+        }
+        if (i < hInd) {
+            ordenarPuntosPorX(i, hInd);
+        }
+    }
+
+    private int[] obtenerCercanos(int lInd, int hInd, int x){
+        int length = hInd - lInd;
+        if (length > 2){
+            int[] mitadDer = obtenerCercanos(lInd, x, lInd+(x-lInd)/2);
+            int[] mitadIzq = obtenerCercanos(x, hInd, x+(hInd-x)/2);
+
+            int[] cercanos = minimo(mitadDer, mitadIzq);
+            long distanciaMinima = distancia(cercanos[0], cercanos[1]);
+
+            Integer[] candidatos = buscarCandidatos(lInd, hInd, x, distanciaMinima);
+            for (int punto1 = 0; punto1 < candidatos.length; punto1++){
+                for (int punto2 = 0; punto2 < candidatos.length; punto2++){
+                    long dist = distancia(candidatos[punto1], candidatos[punto2]);
+                    if (dist < distanciaMinima){
+                        distanciaMinima = dist;
+                        cercanos[0] = punto1;
+                        cercanos[1] = punto2;
+                    }
+                }
+            }
+            return cercanos;
+        } else {
+            return new int[]{lInd, hInd};
+        }
+    }
+
+    private int[] minimo(int[] par1, int[] par2){
+        long d1 = distancia(par1[0], par1[1]);
+        long d2 = distancia(par2[0], par2[1]);
+
+        if (d1 == 0 && d2 == 0){
+            return new int[]{par1[0], par2[0]};
+        } else if (d1 == 0){
+            return par2;
+        } else if (d2 == 0){
+            return par1;
+        } else if (d1 < d2){
+            return par1;
+        } else if (d2 < d1){
+            return par2;
+        } else{
+            return new int[]{0,0};
+        }
+    }
+
+    private Integer[] buscarCandidatos(int lInd, int hInd, int x, long dMin){
+        ArrayList<Integer> candidatos = new ArrayList<>();
+        //buscar menores de X
+        for(int punto = lInd; punto < x; punto++){
+            if (modelo.getCoordenadaX(x) - modelo.getCoordenadaX(punto) < dMin){
+                candidatos.add(punto);
+            }
+        }
+        //buscar mayores de X
+        for(int punto = hInd; punto > x; punto--){
+            if (modelo.getCoordenadaX(punto) - modelo.getCoordenadaX(x) < dMin){
+                candidatos.add(punto);
+            }
+        }
+        Integer[] ret = new Integer[candidatos.size()];
+        return candidatos.toArray(ret);
     }
 }
